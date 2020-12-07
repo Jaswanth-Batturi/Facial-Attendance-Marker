5efbc1afad33d30b6d2735d8df04f64ab8cb18ff
Jaswanth Batturi <69349723+Jaswanth-Batturi@users.noreply.github.com>
2020-12-07 14:58:58 +0530

    code to get encodings of students images

---
+++ encodings.py
@@ -0,0 +1,39 @@
+# Run this program to encode all the class members images
+
+# It is not necessary to run this code each time while taking
+# attendance unless there are changes made in students profiles.
+
+import cv2
+import face_recognition
+import os
+import pickle
+
+# Folder path of the images
+path = 'ImagesAttendance'
+
+classNames = [] # names
+images = [] # images
+
+# Listing out all the class members profiles in images folder
+profiles = os.listdir(path)
+
+# For each profile, read the image and store the name
+for person in profiles:
+    curImg = cv2.imread(f'{path}/{person}')
+    images.append(curImg) 
+    classNames.append(os.path.splitext(person)[0])
+    
+encodeList = [] # encodings
+
+# Processing the images and getting the encodings
+for img in images:
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    encode = face_recognition.face_encodings(img)[0]
+    encodeList.append(encode)
+    
+data = [classNames, encodeList]
+
+# storing the encodings and names, because it takes a lot of time.
+# if we run this program, to get the encodings of all the profile photos
+# each time while taking attendance.
+pickle.dump(data, open("data.p", "wb"))
